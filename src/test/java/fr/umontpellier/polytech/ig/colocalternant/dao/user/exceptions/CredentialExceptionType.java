5fea11f453100f17f5816590aab9e955dada08b1
Nicolas38144 <nicolas.germani.ndv@gmail.com>
2023-12-15 08:33:16 +0100

    add path for test files

---
+++ src/test/java/fr/umontpellier/polytech/ig/colocalternant/dao/user/exceptions/CredentialExceptionType.java
@@ -0,0 +1,15 @@
+package fr.umontpellier.polytech.ig.colocalternant.dao.user.exceptions;
+
+/**
+ * Enumeration of the types of the credential exceptions.
+ */
+public enum CredentialExceptionType {
+    /*
+     * The email is invalid.
+     */
+    INVALID_EMAIL,
+    /*
+     * The password is invalid.
+     */
+    INVALID_PASSWORD,
+}
