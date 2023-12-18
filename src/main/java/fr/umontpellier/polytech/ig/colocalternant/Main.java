db023d87722c37d6471bcc5c97036dfffc3176f1
Suzanne ROBERT <74824314+HarumiaStar@users.noreply.github.com>
2023-12-18 11:47:46 +0100

    Update Main.java

--- src/main/java/fr/umontpellier/polytech/ig/colocalternant/Main.java
+++ src/main/java/fr/umontpellier/polytech/ig/colocalternant/Main.java
@@ -2,6 +2,6 @@ package fr.umontpellier.polytech.ig.colocalternant;
 
 public class Main {
     public static void main(String[] args) {
-        HelloApplication.main(args);
+        App.main(args);
     }
 }
